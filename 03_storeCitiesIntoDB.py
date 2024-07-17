65923c6602bdf2bfd680554e78ec8cf237ac375a
Frankie Law <frankie.sc.law@hotmail.ca>
2024-07-17 15:41:55 -0400

    Minor Edit
    Changes to Database Credentials

--- 03_storeCitiesIntoDB.py
+++ 03_storeCitiesIntoDB.py
@@ -7,7 +7,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="daProject2024#x@to",
+    password="1234",
     database="jobscrappydb"
 )
 
